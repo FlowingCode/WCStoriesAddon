08379402d797b2adb9e9bae49a9031bacc8beaa4
Paola De Bartolo <paola.debartolo@gmail.com>
2021-03-11 10:35:07 -0300

    fix(demo): fix github link

--- src/test/java/com/flowingcode/vaadin/addons/wcstories/WcStoriesDemoView.java
+++ src/test/java/com/flowingcode/vaadin/addons/wcstories/WcStoriesDemoView.java
@@ -26,11 +26,11 @@ import com.vaadin.flow.router.Route;
 
 @SuppressWarnings("serial")
 @Route(value = "wcstories", layout = DemoLayout.class)
-@GithubLink("https://github.com/FlowingCode/WcStoriesAddon")
+@GithubLink("https://github.com/FlowingCode/WCStoriesAddon")
 public class WcStoriesDemoView extends TabbedDemo {
 
 	private static final String WC_STORIES_DEMO = "WC Stories Demo";
-	private static final String WC_STORIES_SOURCE = "https://github.com/FlowingCode/WcStoriesAddon/blob/master/src/test/java/com/flowingcode/vaadin/addons/wcstories/WcStoriesDemo.java";
+	private static final String WC_STORIES_SOURCE = "https://github.com/FlowingCode/WCStoriesAddon/blob/master/src/test/java/com/flowingcode/vaadin/addons/wcstories/WcStoriesDemo.java";
 
 	public WcStoriesDemoView() {
 		addDemo(new WcStoriesDemo(), WC_STORIES_DEMO, WC_STORIES_SOURCE);
